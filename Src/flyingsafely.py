2dfee8c633a1d68e6b8e2f08098c36655acf5200
Bagle <69248303+GenoTheBagle@users.noreply.github.com>
2022-06-28 12:00:22 +0800

    added flying safely
    yeah I don't understand how this worked either

---
+++ Src/flyingsafely.py
@@ -0,0 +1,9 @@
+for i in range(int(input())):
+    n, m = list(map(int, input().split()))
+    # n -> no. of cities
+    # m -> no. of pilots
+    fls = []
+    for i in range(m):
+        input()
+
+    print(n - 1)
