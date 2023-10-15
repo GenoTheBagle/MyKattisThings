3c4651e0d587b19dcad473397e54c04337cb7910
Bagle <69248303+GenoTheBagle@users.noreply.github.com>
2023-10-15 12:16:12 +0800

    added Sith
    October the 15th be with you, wait does that work

---
+++ Src/sith.py
@@ -0,0 +1,15 @@
+input()
+a = int(input())
+b = int(input())
+ab = int(input())
+
+if ab < 0:
+    print("JEDI")
+    exit()
+
+elif a > b:
+    print("VEIT EKKI")
+    exit()
+
+else:
+    print("SITH")
