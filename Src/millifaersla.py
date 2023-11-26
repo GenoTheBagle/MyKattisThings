f89a6d39fc012891d04f619a08979cf3a3cbd049
Bagle <69248303+GenoTheBagle@users.noreply.github.com>
2023-11-26 14:31:23 +0800

    added Millifærsla
    there was a shorter way to do this I did not have the brain capacity today

---
+++ Src/millifaersla.py
@@ -0,0 +1,7 @@
+a = int(input()); b = int(input()); c = int(input())
+if (a < b) and (a < c):
+    print("Monnei")
+elif (b < a) and (b < c):
+    print("Fjee")
+else:
+    print("Dolladollabilljoll")
